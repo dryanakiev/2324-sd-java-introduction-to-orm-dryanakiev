380436017c089bca5490c31f97ecf380feffdb04
Димитър Янакиев <dyanakiev@codingburgas.bg>
2023-12-04 09:26:25 +0200

    Refactor project structure

---
+++ ConnectToDatabase/models/source/Account.java
@@ -0,0 +1,65 @@
+public class Account {
+    int id;
+    String name;
+    String username;
+    String password;
+    String email;
+    String phoneNumber;
+
+    public Account() {
+        id = 0;
+        name = "Not defined";
+        username = "Not defined";
+        password = "Not defined";
+        email = "Not defined";
+        phoneNumber = "Not defined";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ ConnectToDatabase/querries/StorageManagementSystemQuerry.sql
@@ -0,0 +1,117 @@
+CREATE DATABASE StorageManagementSystem
+
+USE StorageManagementSystem
+
+CREATE TABLE Accounts (
+	[Id] INT,
+	[Name] VARCHAR(50),
+	[Username] VARCHAR(50),
+	[Password] VARCHAR(50),
+	[Email] VARCHAR(50),
+	[PhoneNumber] VARCHAR(50)
+);
+
+
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (1, 'Lennard Reddin', 'lreddin0', 'sY7"!Lmpu', 'lreddin0@google.com.au', '804-564-0463');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (2, 'Cam Goldsack', 'cgoldsack1', 'vT3"@<3uhZDeB7', 'cgoldsack1@webnode.com', '620-443-0046');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (3, 'Helyn Crellin', 'hcrellin2', 'uH3>E~|Dn&', 'hcrellin2@va.gov', '501-563-9744');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (4, 'Laurianne Botterill', 'lbotterill3', 'fT0*n#R_{', 'lbotterill3@drupal.org', '676-183-8174');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (5, 'Vincents Faichnie', 'vfaichnie4', 'mV4$1cTe&X0i7i$', 'vfaichnie4@prlog.org', '684-343-1924');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (6, 'Gusty O''Duane', 'goduane5', 'eK1''R#n)l3ueO#', 'goduane5@oaic.gov.au', '653-944-4030');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (7, 'Gallard Menendez', 'gmenendez6', 'pY6/el=/O', 'gmenendez6@cmu.edu', '923-537-8792');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (8, 'Moyna Comazzo', 'mcomazzo7', 'mB2$S?%QNp', 'mcomazzo7@umich.edu', '341-883-2761');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (9, 'Coreen Fulger', 'cfulger8', 'xN9&Gt_<J4O', 'cfulger8@pen.io', '686-209-4049');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (10, 'Micah Handscombe', 'mhandscombe9', 'iN9<u5HK.8a%``', 'mhandscombe9@google.de', '432-307-4211');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (11, 'Prentiss Thraves', 'pthravesa', 'bQ0%OmFC('')bRBG', 'pthravesa@imdb.com', '971-621-6831');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (12, 'Cointon Dungey', 'cdungeyb', 'gF0}Olo1/`kZmUH', 'cdungeyb@nps.gov', '580-388-3666');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (13, 'Guglielmo Mullard', 'gmullardc', 'zM2''Y_jL*6k', 'gmullardc@baidu.com', '981-579-7559');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (14, 'Fina Loadman', 'floadmand', 'uP7?#Ii?a', 'floadmand@home.pl', '169-404-8293');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (15, 'Dimitry Syplus', 'dsypluse', 'cY1)aQ$'',@2m*0Uc', 'dsypluse@craigslist.org', '422-605-7052');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (16, 'Agnesse Watters', 'awattersf', 'xM5)K}$BUubj"', 'awattersf@opera.com', '414-337-4033');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (17, 'Terry Helsdon', 'thelsdong', 'tA3,xvuarUp}.''', 'thelsdong@reddit.com', '520-927-1076');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (18, 'Felix Vondracek', 'fvondracekh', 'mB8&TDhf!iu41_3', 'fvondracekh@cdbaby.com', '595-507-6195');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (19, 'Josephina Gammill', 'jgammilli', 'hP2|Jjb++x+J', 'jgammilli@github.com', '608-424-7261');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (20, 'Sherie O''Dennehy', 'sodennehyj', 'uK7.*4w06"O', 'sodennehyj@nasa.gov', '771-694-1029');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (21, 'Dalston Fynes', 'dfynesk', 'eU2(tGaq', 'dfynesk@bandcamp.com', '490-607-0292');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (22, 'Nydia Lippatt', 'nlippattl', 'fJ5/ZZ)6B*', 'nlippattl@technorati.com', '771-165-4796');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (23, 'Jacquette Argabrite', 'jargabritem', 'bX5+|d@N0', 'jargabritem@jimdo.com', '367-535-3834');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (24, 'Inesita Cambell', 'icambelln', 'gT4)TVikmsj', 'icambelln@wordpress.org', '787-862-6082');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (25, 'Faustina Riby', 'fribyo', 'jU5(OZNpG', 'fribyo@yahoo.co.jp', '593-906-3935');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (26, 'Janis Braferton', 'jbrafertonp', 'oL2%naHN6j86V', 'jbrafertonp@bigcartel.com', '193-420-2551');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (27, 'Linnet Mor', 'lmorq', 'cI2#StCC6dBjs{4,', 'lmorq@shinystat.com', '898-683-3108');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (28, 'Kaylil Myner', 'kmynerr', 'jA0==qgSvBd', 'kmynerr@umich.edu', '340-607-5493');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (29, 'Corilla Stivey', 'cstiveys', 'yO6"qo,rCfW_z', 'cstiveys@artisteer.com', '526-993-3301');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (30, 'Hunfredo Cranston', 'hcranstont', 'sP8_0*E}%~X', 'hcranstont@amazon.co.jp', '427-478-5313');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (31, 'Burgess Orris', 'borrisu', 'qI9|&17#(h@\W', 'borrisu@opensource.org', '209-590-7645');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (32, 'Kaylee Haysham', 'khayshamv', 'zH8{sA>CW+%', 'khayshamv@craigslist.org', '244-934-0294');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (33, 'Leena Abdee', 'labdeew', 'bS1}>b>@|0b', 'labdeew@phpbb.com', '652-362-0645');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (34, 'Wolfy Teasdale', 'wteasdalex', 'eP7>sii{C_<', 'wteasdalex@google.fr', '294-688-4475');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (35, 'Marney Blenkinsop', 'mblenkinsopy', 'bF1&GA<!', 'mblenkinsopy@privacy.gov.au', '922-137-8062');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (36, 'Lawrence Bompas', 'lbompasz', 'oA6''93TT', 'lbompasz@nhs.uk', '479-249-6437');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (37, 'Chad Wendover', 'cwendover10', 'zO9&f/yVfxYUNC', 'cwendover10@va.gov', '411-343-9969');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (38, 'Halley Colwill', 'hcolwill11', 'qG4)3$V1', 'hcolwill11@craigslist.org', '998-772-3307');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (39, 'Yves Telling', 'ytelling12', 'tK2(Mo''WFFw>~'',n', 'ytelling12@csmonitor.com', '615-796-4015');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (40, 'Phaedra Hiorn', 'phiorn13', 'aM4<G5yx,z', 'phiorn13@gmpg.org', '939-328-4006');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (41, 'Bailey Sweedy', 'bsweedy14', 'zA1,wbIKPJyv', 'bsweedy14@utexas.edu', '295-100-1792');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (42, 'Christie Cadagan', 'ccadagan15', 'lC8_E}|{`9~', 'ccadagan15@free.fr', '830-764-9276');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (43, 'Megan De Fraine', 'mde16', 'vY8*)c7%4N?>kV~', 'mde16@paginegialle.it', '695-521-6520');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (44, 'Sheila Shillington', 'sshillington17', 'eW5~.Bqh$''bIRVc/', 'sshillington17@huffingtonpost.com', '190-582-2248');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (45, 'Edouard Taylor', 'etaylor18', 'nW1#yri"Sty+', 'etaylor18@liveinternet.ru', '411-528-4377');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (46, 'Christye Blaxley', 'cblaxley19', 'dF9>y{$S333N', 'cblaxley19@skyrock.com', '765-669-4476');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (47, 'Sherwin Wimlet', 'swimlet1a', 'sV0==`o(yS', 'swimlet1a@altervista.org', '609-549-1094');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (48, 'Erny Levee', 'elevee1b', 'vJ3,5ARoH!%g2', 'elevee1b@tinyurl.com', '305-404-1555');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (49, 'Dalt Mathews', 'dmathews1c', 'hD9|/SQV', 'dmathews1c@simplemachines.org', '681-359-3447');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (50, 'Gabriel Braunfeld', 'gbraunfeld1d', 'zY3}@''0BRzu|//', 'gbraunfeld1d@mozilla.org', '851-471-2950');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (51, 'Elka Labbati', 'elabbati1e', 'gC4_CgK|l', 'elabbati1e@ameblo.jp', '691-482-3736');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (52, 'Myrtia Mate', 'mmate1f', 'tT5+Nsfbd>|c@|67', 'mmate1f@mashable.com', '736-614-9948');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (53, 'Ignacius Dooler', 'idooler1g', 'iK3>v11eWN', 'idooler1g@jigsy.com', '519-581-3350');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (54, 'Thomasin Yakushkin', 'tyakushkin1h', 'iY1/gBO11GvrH{D', 'tyakushkin1h@deliciousdays.com', '350-445-2444');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (55, 'Trip Applebee', 'tapplebee1i', 'pZ2_@&F''Vn', 'tapplebee1i@yale.edu', '239-564-7736');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (56, 'Shamus Broadnicke', 'sbroadnicke1j', 'jH7\dvUEQp', 'sbroadnicke1j@tinyurl.com', '195-698-2150');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (57, 'Clemmie Frankham', 'cfrankham1k', 'bK6.l@R}krmb''', 'cfrankham1k@trellian.com', '166-305-0120');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (58, 'Anneliese Dillway', 'adillway1l', 'hG4%!}0+q.%', 'adillway1l@odnoklassniki.ru', '486-608-1852');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (59, 'Rachelle Jemmison', 'rjemmison1m', 'gJ2*K5fB', 'rjemmison1m@domainmarket.com', '363-855-2129');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (60, 'Serge Ginn', 'sginn1n', 'zQ8%W+BgGSV}BI', 'sginn1n@si.edu', '123-819-5221');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (61, 'Tabbatha Benit', 'tbenit1o', 'yK0@bONmJNPnqX&', 'tbenit1o@epa.gov', '670-644-5441');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (62, 'Gerik Currer', 'gcurrer1p', 'tU6&m&bT_QFXL*vB', 'gcurrer1p@mapquest.com', '637-163-4003');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (63, 'Gerrie Beneteau', 'gbeneteau1q', 'vG8{}p<<yjlI', 'gbeneteau1q@miibeian.gov.cn', '320-580-9278');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (64, 'Marwin Wilcott', 'mwilcott1r', 'lA0*BS%uS*wp0>&S', 'mwilcott1r@apache.org', '551-616-2734');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (65, 'Rebbecca Baglan', 'rbaglan1s', 'gT6<,hLN#/', 'rbaglan1s@i2i.jp', '773-878-5530');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (66, 'Adella Crowley', 'acrowley1t', 'yZ2+(5u`zg~*B', 'acrowley1t@hp.com', '157-958-7217');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (67, 'Jarvis Sparshott', 'jsparshott1u', 'zW6*bniW/Vbr"', 'jsparshott1u@typepad.com', '992-649-9718');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (68, 'Hermann Yuranovev', 'hyuranovev1v', 'lU2~*|zl/xQ0{p', 'hyuranovev1v@shinystat.com', '129-467-8280');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (69, 'Raul Guidotti', 'rguidotti1w', 'dS4`A_v)e%3IOOB', 'rguidotti1w@hubpages.com', '302-297-7705');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (70, 'Tallulah Axcell', 'taxcell1x', 'bY8$Ux#|{(bP3SXk', 'taxcell1x@blogspot.com', '143-553-5075');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (71, 'Ilysa Gerok', 'igerok1y', 'cZ4`Lt@*`ayKl`F,', 'igerok1y@wisc.edu', '749-934-8397');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (72, 'Raeann Cornes', 'rcornes1z', 'mJ8\n`9SQ''t@(}.', 'rcornes1z@gravatar.com', '644-281-8028');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (73, 'Kameko Couves', 'kcouves20', 'fW1}`oR5rTN', 'kcouves20@chicagotribune.com', '330-362-2368');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (74, 'Dwayne Kaaskooper', 'dkaaskooper21', 'jC9~{f4)KWW', 'dkaaskooper21@hp.com', '504-359-8381');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (75, 'Nan Nitto', 'nnitto22', 'xU8@(7lXE+', 'nnitto22@fc2.com', '295-553-4703');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (76, 'Clim Whellans', 'cwhellans23', 'rO7%(|''Q+As|', 'cwhellans23@1und1.de', '824-384-4687');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (77, 'Rivkah Oaks', 'roaks24', 'sP9`>QrDt', 'roaks24@wikimedia.org', '748-924-7070');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (78, 'Ewan Dallyn', 'edallyn25', 'vY6{RxSxg,Mrv}6f', 'edallyn25@disqus.com', '308-827-9045');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (79, 'Wadsworth McCalum', 'wmccalum26', 'mB0*sbQ8acb', 'wmccalum26@ft.com', '982-504-8969');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (80, 'Kale Slemming', 'kslemming27', 'pR5(sBZIiLz&q', 'kslemming27@tiny.cc', '645-927-5510');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (81, 'Leah Berzon', 'lberzon28', 'hL2#bQHSs2Xs$Y.x', 'lberzon28@list-manage.com', '781-209-6152');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (82, 'Shanta Legges', 'slegges29', 'pX6<hgWbWc', 'slegges29@discovery.com', '704-799-6789');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (83, 'Fabiano Rickman', 'frickman2a', 'uE0,qB?3', 'frickman2a@tinypic.com', '680-605-9486');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (84, 'Appolonia D''Andrea', 'adandrea2b', 'hZ8=jwt!', 'adandrea2b@issuu.com', '390-829-3808');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (85, 'Tremayne Parncutt', 'tparncutt2c', 'pH6&9>OdtAn', 'tparncutt2c@europa.eu', '468-761-8791');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (86, 'Royal Tester', 'rtester2d', 'rK1<bBboIg+q9`c', 'rtester2d@netscape.com', '380-608-4241');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (87, 'Sherwin Stillmann', 'sstillmann2e', 'vL9_=<''TqDem', 'sstillmann2e@example.com', '422-239-4879');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (88, 'Blair Graddon', 'bgraddon2f', 'mX7%1{/(vCe&XbUI', 'bgraddon2f@de.vu', '244-518-1333');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (89, 'Alexandrina Ruppeli', 'aruppeli2g', 'cK4`I5DN"tH.{`Ew', 'aruppeli2g@princeton.edu', '328-869-5960');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (90, 'Timmie Dowsing', 'tdowsing2h', 'eR7.JcsB?A8N1A', 'tdowsing2h@instagram.com', '428-571-5516');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (91, 'Lenora Babar', 'lbabar2i', 'gU2$#3Y/phK#5t@', 'lbabar2i@mtv.com', '795-479-1500');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (92, 'Lammond Wharton', 'lwharton2j', 'gQ1|9b9Yx,(#', 'lwharton2j@scientificamerican.com', '422-760-9799');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (93, 'Benny Josupeit', 'bjosupeit2k', 'eK0@g,Ce', 'bjosupeit2k@surveymonkey.com', '119-833-1330');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (94, 'Cordula Inder', 'cinder2l', 'gO4+G''UG?', 'cinder2l@cargocollective.com', '249-197-3669');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (95, 'Lanna Dobrowski', 'ldobrowski2m', 'tN5)ewIF5', 'ldobrowski2m@webmd.com', '480-169-9198');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (96, 'Tillie Sturr', 'tsturr2n', 'xD2=e(roTM5gjz3', 'tsturr2n@wiley.com', '788-448-0066');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (97, 'Penelopa Pyatt', 'ppyatt2o', 'gP2/qi~Z13S?', 'ppyatt2o@bravesites.com', '412-588-7499');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (98, 'Murielle Quene', 'mquene2p', 'aG0>lGsmT', 'mquene2p@uol.com.br', '897-641-9253');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (99, 'Mona Neads', 'mneads2q', 'tW4!5u5@R', 'mneads2q@yale.edu', '889-560-8237');
+INSERT INTO Accounts ([Id], [Name], [Username], [Password], [Email], [PhoneNumber]) VALUES (100, 'Anne-marie Farbrace', 'afarbrace2r', 'hH7`xz,X6OBY"o', 'afarbrace2r@myspace.com', '325-570-7475');
+
+
+SELECT * FROM Accounts
