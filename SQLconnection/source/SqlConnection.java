563a41709920aab7c5d67f8e763fa30ce4f28e01
Димитър Янакиев <dyanakiev@codingburgas.bg>
2023-12-04 09:22:05 +0200

    Add data model and Read query

--- SQLconnection/source/SqlConnection.java
+++ SQLconnection/source/SqlConnection.java
@@ -1,45 +1,31 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SqlConnection {
 
-    private static Connection connectionInstance = null;
+    // CRUD - Create / Read / Update / Delete
 
-    private SqlConnection() {
+    // Read query
+    public List<Account> getAccounts() {
+        List<Account> accounts = new ArrayList<>();
+        Account account = new Account();
+
+        String query = "SELECT * FROM Accounts";
+
+        Connection connectionInstance;
+        Statement statement;
+        ResultSet set;
+
+        try {
+            connectionInstance = DBConnection.getInstance();
+            statement = connectionInstance.createStatement();
+            set = statement.executeQuery(query);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
-
-    public static Connection getInstance() {
-        if (connectionInstance == null) {
-            try {
-                String connectionURL = "jdbc:sqlserver://TL011T\\SQLEXPRESS;database=StorageManagementSystem";
-
-                connectionInstance = DriverManager.getConnection(connectionURL);
-
-                return connectionInstance;
-            } catch (SQLException exception) {
-                throw new RuntimeException(exception);
-            }
-        }
-        else {
-            return connectionInstance;
-        }
-    }
-
-    public static void main(String[] args) throws SQLException {
-        Connection connection = getInstance();
-
-        Statement statement = connection.createStatement();
-
-        ResultSet set = statement.executeQuery("SELECT * FROM StorageManagementSystem");
-
-        while (set.next()) {
-            System.out.println("ID: " + set.getInt(1));
-            System.out.println("Name: " + set.getString(2));
-            System.out.println("Username: " + set.getString(3));
-            System.out.println("Password: " + set.getString(4));
-            System.out.println("Email:" + set.getString(5));
-            System.out.println("Phone number:" + set.getString(6));
-        }
-    }
-
 }
